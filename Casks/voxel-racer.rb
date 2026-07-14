38550b206f0e447c291c5402c2d5a3c4fec02a3e
Stefan Hilbrich <teddyfluffkins@pm.me>
2026-07-14 22:18:31 +0200

    Use public Voxel Racer cask asset

--- Casks/voxel-racer.rb
+++ Casks/voxel-racer.rb
@@ -2,14 +2,15 @@ cask "voxel-racer" do
   version "0.1.0"
   sha256 "00a495597d07eb938aafb2e33fdb3164c415936dbc144731b8b5a8b56bc3ab03"
 
-  url "https://github.com/teddyfluffkins/voxel-racer/releases/download/v#{version}/voxel-racer-v#{version}-macos-unsigned.zip"
+  url "https://github.com/teddyfluffkins/homebrew-tap/releases/download/voxel-racer-v#{version}/voxel-racer-v#{version}-macos-unsigned.zip"
   name "Voxel Racer"
   desc "Small-voxel 3D driving parcours game"
-  homepage "https://github.com/teddyfluffkins/voxel-racer"
+  homepage "https://github.com/teddyfluffkins/homebrew-tap"
 
   livecheck do
-    url :url
-    strategy :github_latest
+    url "https://github.com/teddyfluffkins/homebrew-tap/releases"
+    regex(/href=.*?voxel-racer-v?(\d+(?:\.\d+)+)/i)
+    strategy :page_match
   end
 
   depends_on macos: :big_sur
